bd5eb4942a9d6d12d347ad954e552a5824c9afae
brunosmmm <brunosmmm@gmail.com>
2017-04-27 19:36:03 -0400

    chamged threading arrangements

--- board.py
+++ board.py
@@ -5,6 +5,8 @@ import logging
 import time
 import argparse
 import signal
+from util.threads import StoppableThread
+from util.zeroconf import ZeroconfService
 
 if __name__ == "__main__":
 
@@ -29,31 +31,49 @@ if __name__ == "__main__":
 
     logger = logging.getLogger('sboard')
 
+    #publish
+    avahi_service = ZeroconfService(name='Chainball Scoreboard',
+                                    port=args.port,
+                                    stype='_http._tcp')
+    avahi_service.publish()
+
     logger.info("Scoreboard Starting")
 
     signal.signal(signal.SIGTERM, _handle_signal)
 
-    #create game object
-    game = ChainballGame(virtual_hw=args.nohw)
+    class GameWrapper(StoppableThread):
+
+        def __init__(self, virtual_hw):
+            super(GameWrapper, self).__init__()
+            #create game object
+            self.game = ChainballGame(virtual_hw=virtual_hw)
+            self.game.post_init()
+
+        def run(self):
+            #never exit
+            while True:
+                if self.is_stopped():
+                    self.game.shutdown()
+                    break
+                else:
+                    self.game.game_loop()
+
+                    #throttle main loop
+                    time.sleep(0.01)
+
+    #spawn game engine in thread
+    game_wrapper = GameWrapper(args.nohw)
+    game_wrapper.start()
 
     #web server
-    webScoreBoard = WebBoard(args.port, game, bind_all=True)
+    # NOT THREAD SAFE
+    webScoreBoard = WebBoard(args.port, game_wrapper.game, bind_all=True)
 
     #spawn web server
-    web_server = threading.Thread(target=webScoreBoard.run)
-    web_server.daemon = True
-    web_server.start()
+    webScoreBoard.run()
+    #web_server = threading.Thread(target=webScoreBoard.run)
+    #web_server.daemon = True
+    #web_server.start()
 
-    game.post_init()
-
-    #never exit
-    while True:
-        try:
-            game.game_loop()
-
-            #throttle main loop
-            time.sleep(0.01)
-        except KeyboardInterrupt:
-            game.shutdown()
-            #webScoreBoard.quit()
-            exit(0)
+    game_wrapper.stop()
+    avahi_service.unpublish()
