8e8efd4e60c488adc1b1f3c6352e868f09a0ef31
brunosmmm <brunosmmm@gmail.com>
2017-05-14 17:23:39 -0400

    game/sfxmapper.py flake8

--- game/sfxmapper.py
+++ game/sfxmapper.py
@@ -1,30 +1,52 @@
-import re
+"""Map SFX."""
+
 import json
 
+
 class SFXMappingLoadFailed(Exception):
+    """Could not load mapping."""
+
     pass
+
 
 class SFXUnknownEvent(Exception):
+    """Unknown SFX."""
+
     pass
 
+
 class SFXMappableEvents(object):
+    """Game events which can be mapped."""
+
     GAME_END = 0
     COW_OUT = 1
 
-class SFXMapping(object):
-    MAPPABLE_EVENTS = {"gameEnd" : SFXMappableEvents.GAME_END,
-                       "cowOut" : SFXMappableEvents.COW_OUT}
-    def __init__(self):
 
+class SFXMapping(object):
+    """Mapping logic."""
+
+    MAPPABLE_EVENTS = {"gameEnd": SFXMappableEvents.GAME_END,
+                       "cowOut": SFXMappableEvents.COW_OUT}
+
+    def __init__(self):
+        """Initialize."""
         self.mapping = {}
 
     def load_config(self, filename):
+        """Load configuration.
+
+        Args
+        ----
+        filename: str
+           Path to configuration file
+        """
         try:
             sfx_map_file = open(filename)
             sfx_map = json.loads(sfx_map_file.read())
             sfx_map_file.close()
         except IOError:
-            raise SFXMappingLoadFailed('Could not open sfx mapping configuration file')
+            raise SFXMappingLoadFailed('Could not open '
+                                       'sfx mapping configuration file')
 
         if "sfxmap" not in sfx_map:
             raise SFXMappingLoadFailed('Configuration is invalid')
@@ -36,6 +58,13 @@ class SFXMapping(object):
             self.mapping[self.MAPPABLE_EVENTS[key]] = value
 
     def get_sfx(self, sfx):
+        """Get SFX by event.
+
+        Args
+        ----
+        sfx: SFXMappableEvents
+           Event
+        """
         if sfx in self.mapping:
             return self.mapping[sfx]
 
