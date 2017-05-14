60913383e16b9fc8d495987753a5809c8f6fd92a
brunosmmm <brunosmmm@gmail.com>
2017-05-13 20:37:10 -0400

    prevent failure if failed loading configuration files. use default

--- game/config.py
+++ game/config.py
@@ -19,17 +19,25 @@ class ChainballGameConfiguration(object):
         self.game_duration = _GAME_DURATION_DEFAULT
 
     def load_config(self, filename):
+        """Load configuration file.
 
+        Args
+        ----
+        filename: str
+           Configuration file path
+        """
         try:
             with open(filename, 'r') as f:
                 config_data = json.load(f)
         except:
             self.logger.warning('Could not load configuration file,'
                                 'using defaults')
+            config_data = {}
 
         # load stuff
         if 'scoreAnnounceInterval' in config_data:
-            self.score_announce_interval = int(config_data['scoreAnnounceInterval'])
+            self.score_announce_interval =\
+                int(config_data['scoreAnnounceInterval'])
 
         if 'pairTimeout' in config_data:
             self.pair_timeout = int(config_data['pairTimeout'])
