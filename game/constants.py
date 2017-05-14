78405482f5c2a40974a0716394be5b7f11ee3e40
brunosmmm <brunosmmm@gmail.com>
2017-05-13 21:00:22 -0400

    formatting

--- game/constants.py
+++ game/constants.py
@@ -1,14 +1,22 @@
+"""Constants."""
+
+
 class GameTurnActions(object):
+    """Possile actions."""
 
     INCREASE_SCORE = 0
     DECREASE_SCORE = 1
     PASS_TURN = 2
 
+
 class MasterRemoteActions(object):
+    """Master remote actions."""
 
     PAUSE_UNPAUSE_CLOCK = 0
 
+
 class GameStates(object):
+    """Game states."""
 
     IDLE = 0
     RUNNING = 1
