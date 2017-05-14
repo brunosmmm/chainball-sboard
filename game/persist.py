3e015e199bcd291788130e65c2fed7bc8b2dcb96
brunosmmm <brunosmmm@gmail.com>
2017-05-14 16:24:29 -0400

    game/persist.py passes flake8

--- game/persist.py
+++ game/persist.py
@@ -1,22 +1,41 @@
+"""Game Persistance."""
+
 import json
 import datetime
 import logging
 from os import listdir
 from os.path import isfile, join
 
+
 class CannotModifyScoresError(Exception):
+    """Unable to modify scores."""
+
     pass
 
+
 class PlayerPersistData(object):
+    """Persistent data for a player."""
+
     def __init__(self, display_name, player_name=None):
+        """Initialize.
+
+        Args
+        ----
+        display_name: str
+           Name shown on display
+        player_name: str
+           Full name, displayed on web interface
+        """
         self.display_name = display_name
         self.player_name = player_name
         self.score = 0
 
     def update_score(self, score):
+        """Update player score."""
         self.score = score
 
     def get_data(self):
+        """Return current data for player."""
         data = {}
 
         data['display_name'] = self.display_name
@@ -25,7 +44,10 @@ class PlayerPersistData(object):
 
         return data
 
+
 class GamePersistStates(object):
+    """Game States."""
+
     RUNNING = 0
     FINISHED = 1
     PAUSED = 2
@@ -34,7 +56,10 @@ class GamePersistStates(object):
              FINISHED: 'FINISHED',
              PAUSED: 'PAUSED'}
 
+
 class GameEventTypes(object):
+    """Events that occur during a game."""
+
     SCORE_CHANGE = 'SCORE_CHANGE'
     SCORE_FORCED = 'SCORE_FORCED'
     COWOUT = 'COWOUT'
@@ -54,10 +79,22 @@ class GameEventTypes(object):
     SLOWPOKE = 'SLOWPOKE'
     SERVE_ADVANCE = 'SERVE_ADVANCE'
 
+
 class GamePersistData(object):
+    """Game persistance data structures."""
 
     def __init__(self, players, handler, current_series):
-        #initialize scores
+        """Initialize.
+
+        Args
+        ----
+        players: dict
+           Dictionary of player data indexed by player number
+        handler: unknown
+           Not sure
+        current_series: int
+           Current game identifier (persistent value always increasing)
+        """
         self.player_data = players
         self.game_state = GamePersistStates.RUNNING
         self.start_time = datetime.datetime.now()
@@ -66,17 +103,36 @@ class GamePersistData(object):
         self.internal_game_id = current_series
         self.user_game_id = None
 
-        #if self.data_change_handler:
-        #    self.data_change_handler()
+        # if self.data_change_handler:
+        #     self.data_change_handler()
 
     def assign_user_id(self, user_id):
+        """Assign an identifier to current game.
+
+        Args
+        ----
+        user_id: str
+           An identifier for this particular game
+        """
         self.user_game_id = user_id
 
         if self.data_change_handler:
             self.data_change_handler()
 
     def update_score(self, player, score, forced_update=False, game_time=None):
+        """Update player score record.
 
+        Args
+        ----
+        player: int
+            Player number
+        score: int
+            New score
+        forced_update: bool
+            Is this a forced update or a normal update; i.e. from scoring evt
+        game_time: int
+            Current game time in seconds
+        """
         if player not in self.player_data:
             raise KeyError('Invalid Player')
 
@@ -97,7 +153,17 @@ class GamePersistData(object):
                 self.data_change_handler()
 
     def force_score(self, player, score, game_time=None):
+        """Force score for a player.
 
+        Args
+        ----
+        player: int
+           Player number
+        score: int
+           New score
+        game_time: int
+           Current game time in seconds
+        """
         if player not in self.player_data:
             raise KeyError('Invalid Player')
 
@@ -114,21 +180,41 @@ class GamePersistData(object):
                         'gtime': game_time})
 
     def start_game(self, remaining_time):
-        #doesnt change the current status for now, but i think it should
+        """Log start of game.
+
+        Args
+        ----
+        remaining_time: int
+           Game duration
+        """
+        # doesnt change the current status for now, but i think it should
         self.log_event(GameEventTypes.GAME_START, {'rtime': remaining_time,
                                                    'gtime': 0})
 
     def end_game(self, reason, winner, running_time, remaining_time):
+        """Log end of game.
 
+        Args
+        ----
+        reason: uknown
+            Reason for end of game
+        winner: int
+            Winner player number
+        running_time: int
+            Current game time in seconds
+        remaining_time: int
+            Remaining time at end, in seconds
+        """
         self.game_state = GamePersistStates.FINISHED
         self.log_event(GameEventTypes.GAME_END, {'reason': reason,
                                                  'winner': winner,
                                                  'gtime': running_time,
                                                  'rtime': remaining_time})
-        #if self.data_change_handler:
-        #    self.data_change_handler()
+        # if self.data_change_handler:
+        #     self.data_change_handler()
 
     def pause_unpause(self):
+        """Pause or unpause game."""
         if self.game_state == GamePersistStates.RUNNING:
             self.game_state = GamePersistStates.PAUSED
             self.log_event(GameEventTypes.GAME_PAUSE, None)
@@ -136,10 +222,21 @@ class GamePersistData(object):
             self.game_state = GamePersistStates.RUNNING
             self.log_event(GameEventTypes.GAME_UNPAUSE, None)
 
-        #if self.data_change_handler:
-        #    self.data_change_handler()
+        # if self.data_change_handler:
+        #     self.data_change_handler()
 
     def log_event(self, evt_type, evt_desc, save=True):
+        """Log an event.
+
+        Args
+        ----
+        evt_type: GameEventTypes
+           Event type
+        evt_desc: str
+           A description for the event
+        save: bool
+           Whether to save to disk immediately
+        """
         self.events.append({'evt_type': evt_type,
                             'evt_desc': evt_desc,
                             'evt_time': str(datetime.datetime.now())})
@@ -148,7 +245,7 @@ class GamePersistData(object):
             self.data_change_handler()
 
     def to_JSON(self):
-
+        """Dump a JSON representation of the current game."""
         json_dict = {}
         json_dict['start_time'] = str(self.start_time)
         json_dict['game_state'] = GamePersistStates.NAMES[self.game_state]
@@ -169,20 +266,29 @@ class GamePersistData(object):
 
         return json_dict
 
+
 class GamePersistance(object):
+    """Game persistance data wrapper class."""
 
     def __init__(self, folder):
+        """Initialize.
 
+        Args
+        ----
+        folder: str
+           Path where game persistance files are stored
+        """
         self.logger = logging.getLogger('sboard.gpersist')
         self.path = folder
         self.game_history = {}
         self.current_game = None
         self.current_game_series = 0
+        self._test_mode = False
 
         self.load_history()
 
     def load_history(self):
-
+        """Load all games saved previously."""
         # load current game number
         try:
             f = open('data/persist/game.json', 'r')
@@ -194,6 +300,7 @@ class GamePersistance(object):
 
         try:
             flist = listdir('./'+self.path)
+            repr(flist)
         except:
             self.logger.error('Could not load individual game persistances')
             return
@@ -206,24 +313,42 @@ class GamePersistance(object):
                     with open(join(self.path, f), 'r') as g:
                         game_data = json.load(g)
                 except:
-                    #raise
-                    self.logger.warning('Could not load game persistance for game {}'.format(f))
+                    # raise
+                    self.logger.warning('Could not load game '
+                                        'persistance for game {}'.format(f))
                     continue
 
                 self.game_history[file_uuid] = game_data
 
     def new_record(self, players):
+        """Create new game record.
+
+        Args
+        ----
+        players: dict
+            Player dictionary
+        """
         game_uuid = '{s:06d}'.format(s=self.current_game_series)
         self.current_game_series += 1
         self.current_game = game_uuid
-        self.game_history[game_uuid] = GamePersistData(players,
-                                                       self.save_current_data,
-                                                       self.current_game_series)
+        self.game_history[game_uuid] =\
+            GamePersistData(players,
+                            self.save_current_data,
+                            self.current_game_series)
         self.save_current_data()
 
         return game_uuid
 
     def log_event(self, evt_type, evt_desc):
+        """Log an event.
+
+        Args
+        ----
+        evt_type: GameEventTypes
+           Event type
+        evt_desc: str
+           Event description
+        """
         try:
             self.game_history[self.current_game].log_event(evt_type,
                                                            evt_desc)
@@ -231,12 +356,32 @@ class GamePersistance(object):
             pass
 
     def start_game(self, remaining_time):
+        """Log start of game.
+
+        Args
+        ----
+        remaining_time: int
+           Game clock duration
+        """
         try:
             self.game_history[self.current_game].start_game(remaining_time)
         except:
             pass
 
     def end_game(self, reason, winner, running_time, remaining_time):
+        """Log end of a game.
+
+        Args
+        ----
+        reason: unknown
+           unknown
+        winner: int
+           Winner player number
+        running_time: int
+           Current game time in seconds
+        remaining_time: int
+           Remaining time in seconds
+        """
         try:
             self.game_history[self.current_game].end_game(reason,
                                                           winner,
@@ -247,22 +392,50 @@ class GamePersistance(object):
         self.current_game = None
 
     def pause_unpause_game(self):
+        """Pause or unpause game."""
         try:
             self.game_history[self.current_game].pause_unpause()
         except:
             pass
 
     def update_current_score(self, player, score, forced_update, game_time):
+        """Update scores.
+
+        Args
+        ----
+        player: int
+           Player number
+        score: int
+           Current score
+        force_update: bool
+           Forced update or not
+        game_time: int
+           Current game time in seconds
+        """
         try:
             self.game_history[self.current_game].update_score(player,
                                                               score,
                                                               forced_update,
                                                               game_time)
         except:
-            self.logger.error('could not update score:'
-            'player {}, score: {}, forced: {}'.format(player, score, forced_update))
+            self.logger.error('could not update score: '
+                              'player {}, score: {}, '
+                              'forced: {}'.format(player,
+                                                  score,
+                                                  forced_update))
 
     def force_current_score(self, player, score, game_time):
+        """Force a score value.
+
+        Args
+        ----
+        player: int
+            Player number
+        score: int
+            New score
+        game_time: int
+            Current game time
+        """
         try:
             self.game_history[self.current_game].force_score(player,
                                                              score,
@@ -271,31 +444,45 @@ class GamePersistance(object):
             pass
 
     def save_current_data(self):
-
+        """Save data immediately."""
         # save game series number
         try:
+            if self._test_mode is True:
+                raise
             with open('data/persist/game.json', 'w') as f:
-                json.dump({'current_series': self.current_game_series}, f, indent=4)
+                json.dump({'current_series': self.current_game_series},
+                          f,
+                          indent=4)
         except:
             self.logger.error('Could not save overall game persistance state')
 
         file_name = join(self.path, self.current_game + '.json')
 
         try:
+            if self._test_mode is True:
+                raise
             with open(file_name, 'w') as f:
-                #f.write(self.game_history[self.current_game].to_JSON())
-                json.dump(self.game_history[self.current_game].to_JSON(), f, indent=4)
+                json.dump(self.game_history[self.current_game].to_JSON(),
+                          f,
+                          indent=4)
         except:
             self.logger.error('Could not save game persistance data')
-            #raise
 
     def assign_user_id(self, user_id):
+        """Assign user id to current game.
+
+        Args
+        ----
+        user_id: str
+           An user defined identifier
+        """
         try:
             self.game_history[self.current_game].assign_user_id(user_id)
         except:
             self.logger.error('Could not assign user id to game')
 
     def get_current_user_id(self):
+        """Get user identifier for current game."""
         try:
             return self.game_history[self.current_game].user_game_id
         except:
