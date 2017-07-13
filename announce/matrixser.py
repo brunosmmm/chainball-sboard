4d6045f6b58c3a13bb3e8f5dd3a8f46ebd102a74
Bruno Morais <brunosmmm@gmail.com>
2017-07-13 13:26:01 -0400

    format matrix controller source

--- announce/matrixser.py
+++ announce/matrixser.py
@@ -1,9 +1,14 @@
+"""Serial LED Matrix controller."""
+
 import serial
 import struct
 import time
 import logging
 
+
 class CommandType(object):
+    """Matrix command types."""
+
     TEXT = 0
     SETPIXEL = 1
     CIRCLE = 2
@@ -14,32 +19,48 @@ class CommandType(object):
     BEGIN = 7
     END = 8
 
+
 class Color(object):
+    """Matrix colors."""
+
     def __init__(self, r, g, b):
+        """Initialize."""
         self.r = r
         self.g = g
         self.b = b
 
     def to_list(self):
+        """Get value as list."""
         return [self.r, self.g, self.b]
 
+
 class Coordinate(object):
+    """Matrix coordinates."""
+
     def __init__(self, x, y):
+        """Initialize."""
         self.x = x
         self.y = y
 
+
 class SerialMessage(object):
+    """Serial command or message."""
+
     def __init__(self, command, data):
+        """Initialize."""
         self.command = command
         self.data = data
 
-class MatrixControllerSerial(object):
-    def __init__(self, serport):
 
+class MatrixControllerSerial(object):
+    """Matrix controller."""
+
+    def __init__(self, serport):
+        """Initialize."""
         self.logger = logging.getLogger('sboard.matrixSer')
 
         try:
-            self.ser_port= serial.Serial(serport, 115200)
+            self.ser_port = serial.Serial(serport, 115200)
             time.sleep(3)
         except:
             self.logger.warning('Timer matrix not present')
@@ -49,12 +70,13 @@ class MatrixControllerSerial(object):
         self.message_buffer = []
 
     def group_messages(self, amount):
+        """Set message group size."""
         self.do_group = amount
 
     def _send_message(self, msg):
 
         bunch = False
-        if self.do_group != None:
+        if self.do_group is not None:
             if self.do_group > 0:
                 self.message_buffer.append(msg)
                 self.do_group -= 1
@@ -94,36 +116,36 @@ class MatrixControllerSerial(object):
 
             msg_buf += section
 
-        if self.ser_port != None:
+        if self.ser_port is not None:
             self.ser_port.write(msg_buf)
-        #print [ord(x) for x in msg_buf]
 
     def setPixel(self, x, y, color):
-
+        """Set a pixel value in the matrix."""
         message = SerialMessage(CommandType.SETPIXEL, [x, y, color])
         self._send_message(message)
 
     def putText(self, color, x, y, text, font, clear=False):
-
-        message = SerialMessage(CommandType.TEXT, [x, y, color, text, font, clear])
+        """Draw text in the matrix."""
+        message = SerialMessage(CommandType.TEXT, [
+            x, y, color, text, font, clear])
         self._send_message(message)
 
     def fill(self, color):
-
+        """Fill matrix."""
         message = SerialMessage(CommandType.FILL, [color])
         self._send_message(message)
 
     def clear(self):
-
+        """Clear matrix."""
         message = SerialMessage(CommandType.CLEAR, [])
         self._send_message(message)
 
     def begin_screen(self):
-
+        """Start screen command."""
         message = SerialMessage(CommandType.BEGIN, [])
         self._send_message(message)
 
     def end_screen(self):
-
+        """End screen command."""
         message = SerialMessage(CommandType.END, [])
         self._send_message(message)
