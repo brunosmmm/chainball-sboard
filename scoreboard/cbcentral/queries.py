b0d4dd7cb1f9f53b3494ce065b331bd39c4d4f7b
Bruno Morais <brunosmmm@gmail.com>
2019-05-15 14:25:34 -0400

    update url

--- scoreboard/cbcentral/queries.py
+++ scoreboard/cbcentral/queries.py
@@ -5,4 +5,4 @@ from scoreboard.cbcentral.api import central_api_get
 
 def query_players():
     """Query registered players from central server."""
-    return central_api_get(sub_api="registry", path="players")
+    return central_api_get(sub_api="api", path="players")
