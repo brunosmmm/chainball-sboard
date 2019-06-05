d6875f6a12cd697a35237cf5def4f59d15f994e9
Bruno Morais <brunosmmm@gmail.com>
2019-06-05 16:57:52 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     install_requires=["pyserial>=3.4", "dbus-python>=1.2.8"],
-    package_data={"data": ["sfx/*"]},
+    package_data={"scoreboard": ["data/*"]},
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="The chainball scoreboard",
