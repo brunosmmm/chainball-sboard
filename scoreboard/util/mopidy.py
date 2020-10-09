4c1c7ebc7240302648f951599b3a42b34079d24e
Bruno Morais <brunosmmm@gmail.com>
2020-10-09 16:12:37 -0400

    Detect if playing

--- scoreboard/util/mopidy.py
+++ scoreboard/util/mopidy.py
@@ -8,6 +8,34 @@ class MopidyError(Exception):
     """Mopidy control error."""
 
 
+def mopidy_is_playing():
+    """Check if currently playing."""
+
+    json_data = {
+        "method": "core.playback.get_state",
+        "jsonrpc": "2.0",
+        "prams": {},
+        "id": 1,
+    }
+    payload = json.dumps(json_data)
+
+    try:
+        ret = requests.post(
+            "http://127.0.0.1:6680/mopidy/rpc",
+            headers={"Content-Type": "application/json"},
+            data=payload,
+        )
+    except requests.exceptions.ConnectionError:
+        raise MopidyError("request failed")
+    except Exception:
+        raise MopidyError("unknown error")
+
+    if ret.status_code != 200:
+        raise MopidyError("request failed")
+
+    return ret.json()["result"] == "playing"
+
+
 def mopidy_pause():
     """Play."""
 
