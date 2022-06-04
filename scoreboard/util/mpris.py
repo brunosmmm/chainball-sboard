4b6a7dc1c80ceb9ece2130a512974dcbebc0abe7
Bruno Morais <brunosmmm@gmail.com>
2022-06-04 08:51:12 -0400

    Implement crude local spotify control

--- scoreboard/util/mpris.py
+++ scoreboard/util/mpris.py
@@ -1,17 +1,39 @@
 """Control local music player using MPRIS."""
 
+import dbus
 from scoreboard.util.musicplayer import MusicPlayer
 
 
 class LocalPlayer(MusicPlayer):
     """Local player."""
 
+    def __init__(self):
+        """Initialize."""
+        bus = dbus.SessionBus()
+        self.proxy = bus.get_object(
+            "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2"
+        )
+        self.player = dbus.Interface(
+            self.proxy, dbus_interface="org.mpris.MediaPlayer2.Player"
+        )
+        self.properties = dbus.Interface(
+            self.proxy, dbus_interface="org.freedesktop.DBus.Properties"
+        )
+
     @property
-    def is_playing():
+    def is_playing(self):
         """Check if playing."""
+        return (
+            self.properties.Get(
+                "org.mpris.MediaPlayer2.Player", "PlaybackStatus"
+            )
+            == "Playing"
+        )
 
-    def pause():
+    def pause(self):
         """Pause."""
+        self.player.Pause()
 
-    def play():
+    def play(self):
         """Play."""
+        self.player.Play()
